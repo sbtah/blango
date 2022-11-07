264c1e84437d3674d5c6b342e0b331260e4172a3
Grzegorz Zygan <gzygan@intel471.com>
2022-11-07 08:45:42 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
+import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug(f"Got {len(posts)} posts")
     return render(request, "blog/index.html", {
         "posts": posts,
     })
@@ -24,6 +29,7 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info(f"Created comment on Post {post.pk} for user {request.user}")
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
